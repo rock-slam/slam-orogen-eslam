5a145bd20517975f15180c45002fee49dc91773f
Jakob Schwendner <jakob.schwendner@dfki.de>
2010-10-29 17:09:18 +0200

    updated the log replay script

--- scripts/process_logs.rb
+++ scripts/process_logs.rb
@@ -18,15 +18,22 @@ Orocos.initialize
 # This will kill processes when we quit the block
 Orocos::Process.spawn('eslam_test') do |p|
     eslam = p.task('eslam')
-    eslam.environment_path = ARGV[1]
-    eslam.configure
-    eslam.start
 
     Orocos.log_all_ports #( {:log_dir => ARGV[0]} )
 
-    log_replay = Pocosim::LogReplay.new( ['lowlevel.1.log'].map!{|x| File.join(ARGV[0], x)} )
+    log_replay = Pocosim::LogReplay.new( ['test.0.log', 'gps.0.log', 'xsens_imu.0.log', 'lowlevel.1.log'].map!{|x| File.join(ARGV[0], x)} )
     log_replay.odometry.odometry_samples.connect_to( eslam.orientation_samples, :type => :buffer, :size => 100 )
     log_replay.odometry.bodystate_samples.connect_to( eslam.bodystate_samples, :type => :buffer, :size => 100 )
+
+    #start_pos = log_replay.gps.position_samples.stream.first[2]
+    #start_pos.orientation = log_replay.xsens_imu.orientation_samples.stream.first[2].orientation
+
+    start_pos = log_replay.pose_estimator.pose_samples.stream[10][2]
+
+    eslam.environment_path = ARGV[1]
+    eslam.start_pose = start_pos
+    eslam.configure
+    eslam.start
 
     widget_grid = WidgetGrid.new
     widget_grid.control( log_replay )
