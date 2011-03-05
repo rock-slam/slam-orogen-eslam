9b60d3ad788e582529ba4a5f4434227d6ee8eea0
Jakob Schwendner <jakob.schwendner@dfki.de>
2011-03-05 17:37:06 +0100

    scripts: rearranged process_logs scripts to be more flexible

---
+++ scripts/eslam.rb
@@ -0,0 +1,75 @@
+require 'vizkit'
+require 'orocos'
+include Orocos
+
+Orocos.initialize
+
+class Eslam
+    def initialize( log_dir, environment_path = nil )
+	@log_dir = log_dir
+	@environment_path = environment_path
+    end
+
+    def start
+	Vizkit.control @log_replay
+	Vizkit.exec
+    end
+
+    def configure
+	config = @eslam.eslam_config
+	config.measurementThreshold.distance = 0.05
+	config.measurementThreshold.angle = 5.0 * Math::PI/180.0
+	config.mappingThreshold.distance = 0.02
+	config.mappingThreshold.angle = 5.0 * Math::PI/180.0
+	config.initialError = 0.5
+	config.particleCount = 250
+	config.minEffective = 150
+	@eslam.eslam_config = config
+
+	start_pos = @log_replay.mb500.position_samples.stream.first[2]
+	start_pos.orientation = @log_replay.xsens_imu.orientation_samples.stream.first[2].orientation
+	@eslam.start_pose = start_pos
+
+	@eslam.environment_path = @environment_path if @environment_path 
+    end
+
+    def run
+	# This will kill processes when we quit the block
+	Orocos::Process.spawn('eslam_test') do |p|
+	    @eslam = p.task('eslam')
+
+	    Orocos.log_all_ports #( {:log_dir => ARGV[0]} )
+
+	    @log_replay = Orocos::Log::Replay.open( @log_dir )
+	    @log_replay.hokuyo.scans.connect_to( @eslam.scan_samples, :type => :buffer, :size => 1000 ) 
+	    @log_replay.odometry.odometry_samples.connect_to( @eslam.orientation_samples, :type => :buffer, :size => 1000 )
+	    @log_replay.odometry.bodystate_samples.connect_to( @eslam.bodystate_samples, :type => :buffer, :size => 1000 )
+	    @log_replay.mb500.position_samples.connect_to( @eslam.reference_pose, :type => :buffer, :size => 10 )
+
+	    configure
+
+	    threshold = 0.5
+	    status_reader = @eslam.streamaligner_status.reader
+
+	    latest_time = nil
+	    @log_replay.align( :use_sample_time )
+	    @log_replay.time_sync do |current_time, actual_time_delta, required_time_delta|
+		module_time = status_reader.read_new
+		if !module_time.nil?
+		    latest_time = module_time.latest_time
+		end
+
+		if latest_time and latest_time + threshold < current_time
+		    0.1
+		else
+		    0
+		end
+	    end
+
+	    @eslam.configure
+	    @eslam.start
+
+	    start
+	end
+    end
+end
--- scripts/process_logs.rb
+++ scripts/process_logs.rb
@@ -1,60 +1,11 @@
 #! /usr/bin/env ruby
-require 'vizkit'
-
-include Orocos
+require 'eslam'
 
 if ARGV.size < 1 then 
     puts "usage: process_logs.rb log_dir [environment_path]"
     exit
 end
 
-BASE_DIR = File.expand_path('..', File.dirname(__FILE__))
-ENV['PKG_CONFIG_PATH'] = "#{BASE_DIR}/build:#{ENV['PKG_CONFIG_PATH']}"
-Orocos.initialize
-
-# This will kill processes when we quit the block
-Orocos::Process.spawn('eslam_test') do |p|
-    eslam = p.task('eslam')
-
-    Orocos.log_all_ports #( {:log_dir => ARGV[0]} )
-
-    log_replay = Orocos::Log::Replay.open( ['hokuyo.0.log','lowlevel.1.log'].map!{|x| File.join(ARGV[0], x)} )
-    log_replay.hokuyo.scans.connect_to( eslam.scan_samples, :type => :buffer, :size => 100 ) 
-    log_replay.odometry.odometry_samples.connect_to( eslam.orientation_samples, :type => :buffer, :size => 100 )
-    log_replay.odometry.bodystate_samples.connect_to( eslam.bodystate_samples, :type => :buffer, :size => 100 )
-
-    config = eslam.eslam_config
-    config.measurementThreshold.distance = 0.02
-    config.measurementThreshold.distance = 0.02
-    config.measurementThreshold.angle = 5.0 * Math::PI/180.0
-    config.mappingThreshold.distance = 0.02
-    config.mappingThreshold.angle = 5.0 * Math::PI/180.0
-    config.particleCount = 1 
-    config.minEffective = 1 
-    eslam.eslam_config = config
-
-    eslam.configure
-    eslam.start
-
-    threshold = 0.2
-    status_reader = eslam.streamaligner_status.reader
-
-    latest_time = nil
-    log_replay.align( :use_sample_time )
-    log_replay.time_sync do |current_time, actual_time_delta, required_time_delta|
- 	module_time = status_reader.read_new
- 	if !module_time.nil?
- 	    latest_time = module_time.latest_time
- 	end
- 
- 	if latest_time and latest_time + threshold < current_time
- 	    0.1
- 	else
- 	    0
- 	end
-    end
-
-    Vizkit.control log_replay
-    Vizkit.exec
-end
+eslam = Eslam.new( ARGV[0], ARGV[1] )
+eslam.run
 
